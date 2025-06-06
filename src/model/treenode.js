49d698edfd078265626ec6e6bde90f611c21654c
areg-77 <aregak10@gmail.com>
2025-06-06 15:58:06 +0300

    reorganized icons

--- src/model/treenode.js
+++ src/model/treenode.js
@@ -1,3 +1,5 @@
+import { iconPath } from './iconPaths.js';
+
 export class BindedProperty {
   constructor(value, onChange) {
     this.onChange = onChange;
@@ -52,11 +54,6 @@ export class BindedProperty {
       });
     }
   }
-}
-
-const iconPath = {
-  file: 'M240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h287q16 0 30.5 6t25.5 17l194 194q11 11 17 25.5t6 30.5v447q0 33-23.5 56.5T720-80H240Zm280-560q0 17 11.5 28.5T560-600h160L520-800v160Z',
-  folder: 'M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h207q16 0 30.5 6t25.5 17l57 57h320q33 0 56.5 23.5T880-640v400q0 33-23.5 56.5T800-160H160Z'
 }
 
 export class TreeNode {
